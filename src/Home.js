35f53ec27e48e39af078243f19bd66a2fdb106cf
Vinh Nguyen <developerthienkim@gmail.com>
2020-06-02 20:23:20 +0700

    Add content for home page

--- src/Home.js
+++ src/Home.js
@@ -1,44 +1,91 @@
 import React from 'react';
-import {
-  Card, Button, CardImg, CardTitle, CardText, CardDeck,
-  CardSubtitle, CardBody
-} from 'reactstrap';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebookF, faTwitter, faPinterest, faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons";
 
-import card1 from './assets/card-1.jpg';
-import card2 from './assets/card-2.jpg';
-import card3 from './assets/card-3.jpg';
-
-const titles = [
-  {
-    title: 'Meeting',
-    image: card1
-  },
-  {
-    title: 'Music',
-    image: card2
-  },
-  {
-    title: 'Trip',
-    image: card3
-  }
-];
+import group from './assets/group.jpg';
+import travel from './assets/travel.jpg';
+import cook from './assets/cooking.jpg';
+import desk from './assets/desk.jpg';
 
 export default function Home() {
   return (
-    <CardDeck>
-      {titles.map(item => (
-        <Card>
-          <CardImg top width="100%" src={item.image} alt="Card image cap" />
-          <CardBody>
-            <CardTitle>{item.title}</CardTitle>
-            <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-            <CardText>
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </CardText>
-          </CardBody>
-        </Card>
-      ))}
-      
-    </CardDeck>
+    <div>
+      <div className='container-fluid padding'>
+        <div className='row padding'>
+          <div className='col-md-12 col-lg-6'>
+            <h2>If you build it...</h2>
+            <p>Bootstrap Accordions makes it possible for designers to effectively organize contents within collapsible items. </p>
+            <p>Lo! in the orient when the gracious light Lifts up his burning head, each under eye Doth homage to his new-appearing sight, Serving with looks his sacred majesty; And having climb'd the steep-up heavenly hill, Resembling strong youth in his middle age, Yet mortal looks adore his beauty still</p>
+            <p>Bootstrap Accordions makes it possible for designers to effectively organize contents within collapsible items. </p>
+            <br/>
+            <a href='#' className='btn btn-primary'>Learn More</a>
+          </div>
+          <div className='col-lg-6'>
+            <img src={desk} className='img-fluid' />
+          </div>
+        </div>
+      </div>
+
+      <div className='container-fluid padding pt-5'>
+        <div className='row welcome text-center'>
+          <div className='col-12'>
+            <h1 className='display-4'>Group near you</h1>
+          </div>
+          <hr/>
+        </div>
+      </div>
+
+      <div className='container-fluid padding pt-5'>
+        <div className='row padding'>
+          <div className='col-md-4'>
+            <div className='card'>
+              <img className='card-img-top' src={group} />
+              <div className='card-body'>
+                <h4 className='card-title'>Meeting</h4>
+                <p className='card-text'>Playing board games is the greatest way to meet people and have some fun! We have some Bang and Bang Dice</p>
+                <a href='#' className='btn btn-outline-secondary'>See more</a>
+              </div>
+            </div>
+          </div>
+
+          <div className='col-md-4'>
+            <div className='card'>
+              <img className='card-img-top' src={travel} />
+              <div className='card-body'>
+                <h4 className='card-title'>Travel</h4>
+                <p className='card-text'>Playing board games is the greatest way to meet people and have some fun! We have some Bang and Bang Dice</p>
+                <a href='#' className='btn btn-outline-secondary'>See more</a>
+              </div>
+            </div>
+          </div>
+
+          <div className='col-md-4'>
+            <div className='card'>
+              <img className='card-img-top' src={cook} />
+              <div className='card-body'>
+                <h4 className='card-title'>Cooking</h4>
+                <p className='card-text'>Playing board games is the greatest way to meet people and have some fun! We have some Bang and Bang Dice</p>
+                <a href='#' className='btn btn-outline-secondary'>See more</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='container-fluid padding pt-5'>
+        <div className='row text-center padding'>
+          <div className='col-12'>
+            <h2>Connect</h2>
+          </div>
+          <div className='col-12 social padding pt-5'>
+            <a href='#'> <FontAwesomeIcon icon={faFacebookF} /></a>
+            <a href='#'> <FontAwesomeIcon icon={faTwitter} /></a>
+            <a href='#'> <FontAwesomeIcon icon={faPinterest} /></a>
+            <a href='#'> <FontAwesomeIcon icon={faInstagram} /></a>
+            <a href='#'> <FontAwesomeIcon icon={faYoutube} /></a>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
